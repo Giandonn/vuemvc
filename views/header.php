9465a0e5d40a5f9cdc8bed4351c47af35cd4f8a1
Giandonn <lucas_raineri04@hotmail.com>
2024-02-07 10:58:20 -0300

    update

--- views/header.php
+++ views/header.php
@@ -60,7 +60,7 @@
     <!--========== HEADER ==========-->
     <header class="header">
         <div class="header__container">
-            <a href="#" class="header__logo">Estrutura MVC - VUE</a>
+            <a href="#" class="header__logo">Estrutura MVC - VUE by Lucas Raineri</a>
 
             <a href="#" class="header__logo"><?php if (isset($_SESSION['nome'])){
                 echo($_SESSION['nome']);
